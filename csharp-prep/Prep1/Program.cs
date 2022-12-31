4944d4143dd04d656d9236c046d5a7f7eb800b68
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2022-12-31 18:56:26 -0300

    Assigment 1 - completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /* Input to receive the first name */
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine(); /* Save variable */
+        /* Input to receive the last name */
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine(); /* Save variable */
+
+
+        Console.WriteLine();/* Space */
+        
+        /* Output*/
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
