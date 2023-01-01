cdf22e1ce99562c8e99f8041924b667d5d37fba8
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2022-12-31 21:20:03 -0300

    Prep3 assigment - finish

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /* Get the magic number and convert to intenger */
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int guessNumberConverted; //Declarate the variable to work inside the scope 
+
+        do
+        {
+        /* Get a guess and convert to intenger */
+        Console.Write("What is your guess? ");
+        string guessNumber = Console.ReadLine();
+        guessNumberConverted = int.Parse(guessNumber);
+
+        
+            if (magicNumber > guessNumberConverted){
+                Console.WriteLine("Higher");
+            }
+
+            else if (magicNumber < guessNumberConverted){
+                Console.WriteLine("Lower");
+            }
+        } 
+
+        //Loop to repeat the guest until guess the correct number
+        while (magicNumber != guessNumberConverted);
+        
+        //Final message
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine(); //Space
     }
 }
