8cfdcdd5f3098a11ece4d3882a286fb8919d5293
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-01-07 12:52:30 -0300

    Clear commits

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,80 +4,87 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Inputs
+        Console.Write("Enter the book: ");
+        string userBook = Console.ReadLine();
+        Console.Write("Enter the chapter of the book: ");
+        string userChapter = Console.ReadLine();
+        Console.Write("Enter the first verse of the chapter: ");
+        string userVerseF = Console.ReadLine();
+        Console.Write("Enter the last verse of the chapter: (Press 'N' if there isn't) ");
+        string userVerseL = Console.ReadLine().ToLower();
+        Console.Write("Enter your scripture: ");
+        string userPhase = Console.ReadLine();
 
-Console.Write("Enter the book: ");
-string userBook = Console.ReadLine();
-Console.Write("Enter the chapter of the book: ");
-string userChapter = Console.ReadLine();
-Console.Write("Enter the first verse of the chapter: ");
-string userVerseF = Console.ReadLine();
-Console.Write("Enter the last verse of the chapter: (Press 'N' if there isn't) ");
-string userVerseL = Console.ReadLine().ToLower();
-Reference referenceStructure = new(userBook, userChapter, userVerseF, userVerseL);
-string reference = referenceStructure.GetCompleteReference();
-Console.Write("Enter your scripture: ");
-string userPhase = Console.ReadLine();
-Scripture scritureStructure = new Scripture(userPhase);
-string scripture = scritureStructure.GetCompleteScripture();
-
-Domain domain = new Domain(reference, scripture);
-string CompletePhrase = domain.FraseJunta();
-
-
-List<string> lista = new();
-lista = scripture.Split(" ").ToList();
-
-List<string> listWithSlash = new();
-bool condition = true;
+        //Create new objetcs for the reference, the scripture, and the domain
+        Reference referenceStructure = new(userBook, userChapter, userVerseF, userVerseL);
+        string reference = referenceStructure.GetCompleteReference();
+        Scripture scritureStructure = new Scripture(userPhase);
+        string scripture = scritureStructure.GetCompleteScripture();
+        Domain domain = new Domain(reference, scripture);
+        string CompletePhrase = domain.FraseJunta();
 
 
-Console.WriteLine($"Original quote: {CompletePhrase}");
-while (lista.Any(s => !s.All(c => c == '-')) && condition)
-{
-    Random rnd = new Random();
-    int index = rnd.Next(0, lista.Count);
-    string word = lista[index];
+        List<string> lista = new();
+        lista = scripture.Split(" ").ToList();
 
-    string newWord = "";
+        //A list will change to "-" 
+        List<string> listWithSlash = new();
+        bool condition = true;
 
-    for (int i = 0; i < word.Length; i++)
-    {
-        newWord += "-";
-    }
 
-    lista[index] = newWord;
+        Console.WriteLine($"Original quote: {CompletePhrase}");
+        while (lista.Any(s => !s.All(c => c == '-')) && condition)
+        {
+            //Create a random library to change the place of the word will turn to "-"
+            Random rnd = new Random();
+            int index = rnd.Next(0, lista.Count); //0, max elements in the list (range)
+            string word = lista[index];
 
-    string UltimatePhase = string.Empty;
-    for (int i = 0; i < lista.Count; i++)
-    {
-        UltimatePhase += lista[i].ToString() + " ";
-    }
+            string newWord = "";
 
-    Console.WriteLine($"Updated quote: {reference} {UltimatePhase}");
-    Console.WriteLine("If you want to continue press the 'Enter' otherwise type 'Exit' ");
-    string response = Console.ReadLine().ToLower();
+            //Do the action to all the letters of the word
+            for (int i = 0; i < word.Length; i++)
+            {
+                newWord += "-";
+            }
 
-    if (response == "exit")
-    {
-        condition = false;
-        Console.WriteLine($"The last instance reached was:");
-    }
-    Console.Clear();
-}
+            lista[index] = newWord;
 
-listWithSlash.Add(reference + " ");
+            string UltimatePhase = string.Empty;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                UltimatePhase += lista[i].ToString() + " ";
+            }
 
-for (int i = 0; i < lista.Count; i++)
-{
-    listWithSlash.Add((string)lista[i] + " ");
-}
+            Console.WriteLine($"Updated quote: {reference} {UltimatePhase}");
 
-foreach (var item in listWithSlash)
-{
-    Console.Write($"{item}");
-}
+            Console.WriteLine("If you want to continue press the 'Enter' otherwise type 'Exit' ");
+            string response = Console.ReadLine().ToLower();
 
-Console.WriteLine($"Original quote: {CompletePhrase}");
+            if (response == "exit")
+            {
+                //Change the concition to stop the loop and print the last message
+                condition = false;
+            }
+            Console.Clear();
+        }
+
+        listWithSlash.Add(reference + " ");
+
+        for (int i = 0; i < lista.Count; i++)
+        {
+            listWithSlash.Add((string)lista[i] + " ");
+        }
+
+        foreach (var item in listWithSlash)
+        {
+            Console.Write($"{item}");
+        }
+
+        //Clean the console and print the final last output */
+        Console.Clear();
+        Console.WriteLine($"Original quote: {CompletePhrase}");
 
     }
 }
